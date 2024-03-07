a8fbc9588365262a9db51a914ea6b6bdcb910ae7
dpopov75 <160723862+dpopov75@users.noreply.github.com>
2024-03-06 19:26:57 -0700

    Updated coffeeshop.py

--- coffeeshop.py
+++ coffeeshop.py
@@ -1,24 +1,35 @@
 #Daniel Popov
 #Coffee Shop Script
-#Asks how many coffees and muffins the user would like to buy and calculates a total.
-print('Coffee and Muffin Shop')
-print('$5 per coffee and $4 per muffin')
-Coffee = (input('''How many coffees would you like to buy?
-'''))
-Muffins = (input('''How many muffins would you like to buy?
-'''))
+#Asks how many coffees, muffins, brownies, and burritos the user would like to buy and calculates a total based on pre-set prices
+print("Daniels Cafe")
+print('$5 per coffee, $4 per muffin, $6 per brownie, $2.50 per burrito')
+Coffee = input('''How many coffees would you like to buy?
+''')
+Muffin = input('''How many muffins would you like to buy?
+''')
+Brownie = input('''How many brownies would you like to buy?
+''')
+Burrito = input('''How many burritos would you like to buy?
+''')
 NumCof = float(Coffee)
-NumMuf = float(Muffins)
+NumMuf = float(Muffin)
+NumBro = float(Brownie)
+NumBur = float(Burrito)
 CofCost = (NumCof * 5)
 MufCost = (NumMuf * 4)
-TaxCost = (CofCost + MufCost)* 0.06
+BroCost = (NumBro * 6)
+BurCost = (NumBur * 2.5)
+TaxCost = (CofCost + MufCost + BroCost + BurCost)* 0.06
 TaxFinal = round(TaxCost, 2)
-TotalCost = CofCost + MufCost + TaxFinal
-print('My Coffee and Muffin Shop Receipt')
+TotalCost = CofCost + MufCost + BroCost + BurCost + TaxFinal
+print('My Cafe Receipt')
 print(Coffee,'Coffee at $5 each: $', CofCost)
-print(Muffins, 'Muffins at $4 each: $', MufCost)
+print(Muffin, 'Muffins at $4 each: $', MufCost)
+print(Brownie, 'Brownies at $6 each: $', BroCost)
+print(Burrito, 'Burritos at $2.50 each: $', BurCost)
 print('6% tax: $', TaxFinal)
 print(' ----------')
 print('Total: $' , TotalCost)
+print("Thank you for shopping")
 
 
